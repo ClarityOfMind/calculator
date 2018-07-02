5cc19db3ea79e66f66928d56c20398068b92a3b7
Dmitry Vasilev <zastrupen@gmail.com>
2018-07-02 22:22:06 +0300

    Final commit

--- src/js/index.js
+++ src/js/index.js
@@ -14,15 +14,10 @@ const calculate = (operand1, operator, operand2) => {
     let Value1 = parseFloat(operand1);
     let Value2 = parseFloat(operand2);
 
-    if (operator === 'add') {
-        return Value1 + Value2
-    } else if (operator === 'subtract') {
-        return Value1 - Value2
-    } else if (operator === 'multiply') {
-        return Value1 * Value2
-    } else if (operator === 'divide') {
-        return Value1 / Value2
-    }
+    if (operator === 'add') {return Value1 + Value2};
+    if (operator === 'subtract') {return Value1 - Value2};
+    if (operator === 'multiply') {return Value1 * Value2};
+    if (operator === 'divide') {return Value1 / Value2};
 }
 
 keys.addEventListener ('click', e => {
